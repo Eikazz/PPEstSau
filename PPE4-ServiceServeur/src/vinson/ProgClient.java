d5f86042982e5545e89928287a178b5285b14978
VINSON <VINSON@HOS5UC19.st-sau.fr>
2014-03-27 10:13:12 +0100

    Boucle répeter ajouter, Le code gèrer maintenant correctement l'ajout du nombre aléatoire

--- PPE4-ServiceServeur/src/vinson/ProgClient.java
+++ PPE4-ServiceServeur/src/vinson/ProgClient.java
@@ -1,15 +1,19 @@
 package vinson;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgClient {
 	
 	 public static void main(String args[]) {
 		 
+		 
 		   String url = "jdbc:odbc:TEST";
 		   Connection con = null;
 		   NombreAlea unNombreAlea=new NombreAlea();
 		   int numAjout=unNombreAlea.getNombreAlea();
-		   boolean verif=false;
+		   boolean trouver;
+		   int taille=10;
 		   
 		   try {Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 		     	con = DriverManager.getConnection(url,"","");
@@ -17,25 +21,37 @@ public class ProgClient {
 		        
 		        ResultSet resultatDest = requete.executeQuery("select * from NombreAlea");
 		        
-		        while (resultatDest.next() && verif==false) {
-		        	if (numAjout==resultatDest.getInt(1)){
-		        		verif=true;
-		        	}
-                    };
-			if (verif==false){
-		           int  NbIns ;
-		              NbIns = requete.executeUpdate("INSERT INTO NombreAlea (NombreAlea) VALUES ("+numAjout+") ");
-		            System.out.println(NbIns+" ligne insérée");}
-			else
-			{
-				
-				unNombreAlea.ChangeAlea();
-				numAjout=unNombreAlea.getNombreAlea();
-				while(resultatDest.next() && verif==true)
-				{}
-			}
-		            
-		        	
+		        ArrayList ListNombre = new ArrayList() ; 
+
+		        while(resultatDest.next()) {
+
+		        	ListNombre.add(resultatDest.getInt(1)); 
+		        }
+		        if (ListNombre.size()!=taille)
+		        {
+			        do
+			        {
+			        	trouver=false;
+				        for(int i=0;i<ListNombre.size();i++)
+				        {
+				        	if((ListNombre.get(i).equals(numAjout)))
+				        	{
+				        		i=ListNombre.size();
+				        		trouver=true;
+				        		unNombreAlea.ChangeAlea();
+								numAjout=unNombreAlea.getNombreAlea();
+				        	}
+				        }
+			        }
+			        while(trouver);  
+			        int NbIns;
+			        NbIns = requete.executeUpdate("INSERT INTO NombreAlea (NombreAlea) VALUES ("+numAjout+") ");
+			        System.out.println(NbIns+" ligne insérée");
+		        }
+		        else{
+		        System.out.println("Nombre d'enregistrement maximum atteint");
+		        }
+		            	        	
 		       }
 		   
 		  catch(Exception e) {  System.out.println("Exception"+e);  }
@@ -44,4 +60,4 @@ public class ProgClient {
 		    catch(SQLException e) {e.printStackTrace();}
 		    }
 		}
-		}
+	}
